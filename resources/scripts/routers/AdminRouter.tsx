d367b2800e79ba62a9e5cb751ebf74ec758bc780
ikketim <info@timvds.nl>
2025-11-05 20:54:41 +0100

    Update AdminRouter.tsx

--- resources/scripts/routers/AdminRouter.tsx
+++ resources/scripts/routers/AdminRouter.tsx
@@ -40,14 +40,18 @@ function AdminRouter() {
             </MobileSidebar>
             <Sidebar className={'flex-none'} $collapsed={collapsed} theme={theme}>
                 <div
-                    className={'h-16 w-full flex flex-col items-center justify-center my-6 select-none cursor-pointer'}
+                    className={'w-full flex flex-col items-center justify-center my-6 select-none cursor-pointer'}
                     onClick={() => setCollapsed(!collapsed)}
                 >
                     {!collapsed ? (
-                        <h1 className={'text-2xl text-neutral-50 whitespace-nowrap font-medium'}>{settings.name}</h1>
+                        <img
+                            src={settings.logo?.toString() || 'https://ikketim.nl/wp-content/uploads/2025/11/BannerLogo3.png'}
+                            className={'mt-4 w-full h-auto object-contain px-2'}
+                            alt={'Logo'}
+                        />
                     ) : (
                         <img
-                            src={'https://avatars.githubusercontent.com/u/91636558'}
+                            src={settings.logo?.toString() || 'https://ikketim.nl/wp-content/uploads/2025/09/cropped-ikketim-logo-new.png'}
                             className={'mt-4 w-12'}
                             alt={'Logo'}
                         />
